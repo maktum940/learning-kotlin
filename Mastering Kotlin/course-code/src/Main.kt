082c0be58d2408275fb67fe93730ff4631e94f96
maktum940 <mhmaktum006@gmail.com>
2024-02-14 16:09:06 +0600

    Source project for kotlin added

---
+++ Mastering Kotlin/course-code/src/Main.kt
@@ -0,0 +1,7 @@
+import section1.Variable
+
+fun main() {
+    val variable = Variable()
+    variable.variableExercise()
+
+}
---
+++ Mastering Kotlin/course-code/src/section1/Variable.kt
@@ -0,0 +1,12 @@
+package section1
+
+class Variable {
+    fun variableExercise(){
+        val name = "Max"
+        var age = 24
+
+        println("That person's name is: $name. His age is: $age")
+        age += 2
+        println("After 2 year later his name is: $name. But his age is: $age")
+    }
+}
