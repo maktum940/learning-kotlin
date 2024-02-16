1029d081b1ee0118a4063c5f9fb6ffc566bf256c
maktum940 <mhmaktum006@gmail.com>
2024-02-17 01:08:48 +0600

    Learned various loop, function etc no code is updated

--- Mastering Kotlin/course-code/src/Main.kt
+++ Mastering Kotlin/course-code/src/Main.kt
@@ -1,44 +1,58 @@
 import basic.*
 import basic.operator.*
 
+fun greetingWithMyName(name: String){
+    println("Hello! $name. Welcome to kotlin learning")
+}
+
 fun main() {
 
-    println("*--------------------------------* about variable *-----------------------------------------------------*")
-    val variable = Variable()
-    variable.variableAndMathCalculation()
+//    println("*--------------------------------* about variable *-----------------------------------------------------*")
+//    val variable = Variable()
+//    variable.variableAndMathCalculation()
+//
+//    println("*--------------------------------* about data type *----------------------------------------------------*")
+//
+//    val dataType = DataType()
+//    dataType.allTheDataTypeKotlinHas()
+//
+//    println("*--------------------------------* about mathematical operator *----------------------------------------*")
+//
+//    val mathematicalOperator = MathematicalOperator()
+//    mathematicalOperator.operator()
+//
+//    println("*--------------------------------* about Relational operator *------------------------------------------*")
+//
+//    val relationalOperator = RelationalOperator()
+//    relationalOperator.operator()
+//
+//    println("*--------------------------------* about Assignment operator *------------------------------------------*")
+//
+//    val assignmentOperator = AssignmentOperator()
+//    assignmentOperator.operator()
+//
+//    println("*--------------------------------* about Unary operator *-----------------------------------------------*")
+//
+//    val unaryOperator = UnaryOperator()
+//    unaryOperator.operator()
+//
+//    println("*--------------------------------* about Logical operator *-----------------------------------------------*")
+//
+//    val logicalOperator = LogicalOperator()
+//    logicalOperator.operator()
+//
+//    println("*--------------------------------* about Bitwise operator *-----------------------------------------------*")
+//
+//    val bitwiseOperator = BitwiseOperator()
+//    bitwiseOperator.operator()
 
-    println("*--------------------------------* about data type *----------------------------------------------------*")
+//    val myName : String = readln()
+//    println("My name is: $myName")
 
-    val dataType = DataType()
-    dataType.allTheDataTypeKotlinHas()
+    greetingWithMyName(readln())
+    val cars  = arrayOf("Volvo", "Nissan", "Toyota", "Mazda", "porsche")
 
-    println("*--------------------------------* about mathematical operator *----------------------------------------*")
-
-    val mathematicalOperator = MathematicalOperator()
-    mathematicalOperator.operator()
-
-    println("*--------------------------------* about Relational operator *------------------------------------------*")
-
-    val relationalOperator = RelationalOperator()
-    relationalOperator.operator()
-
-    println("*--------------------------------* about Assignment operator *------------------------------------------*")
-
-    val assignmentOperator = AssignmentOperator()
-    assignmentOperator.operator()
-
-    println("*--------------------------------* about Unary operator *-----------------------------------------------*")
-
-    val unaryOperator = UnaryOperator()
-    unaryOperator.operator()
-
-    println("*--------------------------------* about Logical operator *-----------------------------------------------*")
-
-    val logicalOperator = LogicalOperator()
-    logicalOperator.operator()
-
-    println("*--------------------------------* about Bitwise operator *-----------------------------------------------*")
-
-    val bitwiseOperator = BitwiseOperator()
-    bitwiseOperator.operator()
+    for (x in cars){
+        println(x)
+    }
 }
