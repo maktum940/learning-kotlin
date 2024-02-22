9c4e12df47d1eb66fb58e14e907dcf9f7cb8f746
maktum940 <64151360+maktum940@users.noreply.github.com>
2024-02-23 00:44:04 +0600

    Update Main.kt

--- Mastering-Kotlin/course-code/src/Main.kt
+++ Mastering-Kotlin/course-code/src/Main.kt
@@ -7,47 +7,47 @@ fun greetingWithMyName(name: String){
 
 fun main() {
 
-//    println("*--------------------------------* about variable *-----------------------------------------------------*")
-//    val variable = Variable()
-//    variable.variableAndMathCalculation()
-//
-//    println("*--------------------------------* about data type *----------------------------------------------------*")
-//
-//    val dataType = DataType()
-//    dataType.allTheDataTypeKotlinHas()
-//
-//    println("*--------------------------------* about mathematical operator *----------------------------------------*")
-//
-//    val mathematicalOperator = MathematicalOperator()
-//    mathematicalOperator.operator()
-//
-//    println("*--------------------------------* about Relational operator *------------------------------------------*")
-//
-//    val relationalOperator = RelationalOperator()
-//    relationalOperator.operator()
-//
-//    println("*--------------------------------* about Assignment operator *------------------------------------------*")
-//
-//    val assignmentOperator = AssignmentOperator()
-//    assignmentOperator.operator()
-//
-//    println("*--------------------------------* about Unary operator *-----------------------------------------------*")
-//
-//    val unaryOperator = UnaryOperator()
-//    unaryOperator.operator()
-//
-//    println("*--------------------------------* about Logical operator *-----------------------------------------------*")
-//
-//    val logicalOperator = LogicalOperator()
-//    logicalOperator.operator()
-//
-//    println("*--------------------------------* about Bitwise operator *-----------------------------------------------*")
-//
-//    val bitwiseOperator = BitwiseOperator()
-//    bitwiseOperator.operator()
+    println("*--------------------------------* about variable *-----------------------------------------------------*")
+    val variable = Variable()
+    variable.variableAndMathCalculation()
 
-//    val myName : String = readln()
-//    println("My name is: $myName")
+    println("*--------------------------------* about data type *----------------------------------------------------*")
+
+    val dataType = DataType()
+    dataType.allTheDataTypeKotlinHas()
+
+    println("*--------------------------------* about mathematical operator *----------------------------------------*")
+
+    val mathematicalOperator = MathematicalOperator()
+    mathematicalOperator.operator()
+
+    println("*--------------------------------* about Relational operator *------------------------------------------*")
+
+    val relationalOperator = RelationalOperator()
+    relationalOperator.operator()
+
+    println("*--------------------------------* about Assignment operator *------------------------------------------*")
+
+    val assignmentOperator = AssignmentOperator()
+    assignmentOperator.operator()
+
+    println("*--------------------------------* about Unary operator *-----------------------------------------------*")
+
+    val unaryOperator = UnaryOperator()
+    unaryOperator.operator()
+
+    println("*--------------------------------* about Logical operator *-----------------------------------------------*")
+
+    val logicalOperator = LogicalOperator()
+    logicalOperator.operator()
+
+    println("*--------------------------------* about Bitwise operator *-----------------------------------------------*")
+
+    val bitwiseOperator = BitwiseOperator()
+    bitwiseOperator.operator()
+
+    val myName : String = readln()
+    println("My name is: $myName")
 
     greetingWithMyName(readln())
     val cars  = arrayOf("Volvo", "Nissan", "Toyota", "Mazda", "porsche")
